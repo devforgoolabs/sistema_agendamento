aa119be313b6fa84bc120f133d7e71a85febd742
devforgoolabs <brenosumaeta33@gmail.com>
2021-09-21 18:38:18 -0300

    Finaly model domain

---
+++ src/main/java/com/goolab/models/Agendamento.java
@@ -0,0 +1,126 @@
+package com.goolab.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Agendamento implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Prestador prestador;
+	private Unidade unidade;
+	private Especialidade especialidade;
+	private Servico servico;
+	private PlanoSaude planoSaude;
+	private Horario horario;
+	private Paciente paciente;
+	
+	public Agendamento() {
+
+	}
+
+	public Agendamento(Long id, Prestador prestador, Unidade unidade, Especialidade especialidade, Servico servico,
+			PlanoSaude planoSaude, Horario horario, Paciente paciente) {
+		this.id = id;
+		this.prestador = prestador;
+		this.unidade = unidade;
+		this.especialidade = especialidade;
+		this.servico = servico;
+		this.planoSaude = planoSaude;
+		this.horario = horario;
+		this.paciente = paciente;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Prestador getPrestador() {
+		return prestador;
+	}
+
+	public void setPrestador(Prestador prestador) {
+		this.prestador = prestador;
+	}
+
+	public Unidade getUnidade() {
+		return unidade;
+	}
+
+	public void setUnidade(Unidade unidade) {
+		this.unidade = unidade;
+	}
+
+	public Especialidade getEspecialidade() {
+		return especialidade;
+	}
+
+	public void setEspecialidade(Especialidade especialidade) {
+		this.especialidade = especialidade;
+	}
+
+	public Servico getServico() {
+		return servico;
+	}
+
+	public void setServico(Servico servico) {
+		this.servico = servico;
+	}
+
+	public PlanoSaude getPlanoSaude() {
+		return planoSaude;
+	}
+
+	public void setPlanoSaude(PlanoSaude planoSaude) {
+		this.planoSaude = planoSaude;
+	}
+
+	public Horario getHorario() {
+		return horario;
+	}
+
+	public void setHorario(Horario horario) {
+		this.horario = horario;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Agendamento other = (Agendamento) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
