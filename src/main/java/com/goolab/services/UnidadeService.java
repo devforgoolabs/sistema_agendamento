b08a65585045a481c50be95b58311ee47dcdd361
devforgoolabs <brenosumaeta33@gmail.com>
2021-09-22 11:43:13 -0300

    Criando a classe de serviço

---
+++ src/main/java/com/goolab/services/UnidadeService.java
@@ -0,0 +1,12 @@
+package com.goolab.services;
+
+import com.goolab.models.Unidade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UnidadeService {
+
+    @Autowired
+    private Unidade unidade;
+}
